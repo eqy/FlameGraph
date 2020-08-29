df8f890a08112ff5513ea03274796f5af52c670f
Michael Carilli <mcarilli@gmail.com>
2020-08-28 18:11:06 -0600

    comment

--- torch/main.cpp
+++ torch/main.cpp
@@ -27,15 +27,17 @@ int main() {
   micro_requires_grad_true();
   micro_requires_grad_false();
 
-  // The two calls below help illustrate flamegraph's aggregation policy.
-  // All samples with the same call chain get lumped together on the flamegraph.
-  // The flamegraph shows each section with width proportional to the number of samples matching that particular call chain.
-  // Therefore, these two separate invocations of subfunc will appear as one big "subfunc" section of the flame graph.
-  // Their subcalls (micro_requires_grad_true() and micro_requires_grad_false()) are similarly aggregated.
-  // The "micro_requires_grad_true/false" sections within the "subfunc" section will appear
-  // roughly twice as wide as main's individual direct micro_requires_grad_true/false calls above, because roughly twice
-  // as many samples should hit "main->subfunc->micro_requires_grad_*" call chains as hit the
-  // "main->micro_requires_grad_*" call chains.
+  /* The two calls below help illustrate flamegraph's aggregation policy.
+
+  All samples with the same call chain get lumped together on the flamegraph.
+  The flamegraph shows each section with width proportional to the number of samples matching that particular call chain.
+
+  Therefore, these two separate invocations of subfunc will appear as one big "subfunc" section of the flame graph.
+  Their subcalls (micro_requires_grad_true() and micro_requires_grad_false()) are similarly aggregated.
+  The "micro_requires_grad_true/false" sections within the "subfunc" section will appear
+  roughly twice as wide as main's individual direct micro_requires_grad_true/false calls above, because roughly twice
+  as many samples should hit "main->subfunc->micro_requires_grad_*" call chains as hit the
+  "main->micro_requires_grad_*" call chains.  */
   subfunc();
   subfunc();
 }
